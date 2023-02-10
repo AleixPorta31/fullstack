be7d41faf12d171cdd7bf56db43dc84e4d214e12
AleixPorta31 <aleixportaeij@gmail.com>
2023-02-10 14:43:45 +0100

    dia 10

--- JavaScript/strings.js
+++ JavaScript/strings.js
@@ -16,4 +16,75 @@
 // let frase = 'lorem ipsum daltae premta sicnoc lorem preta lorem ipsum';
 // document.write(frase.replaceAll('lorem', 'lolololo'));
 
+// let palabra = "sometemos";
+// let inverso ="";
+
+// for (let i = palabra.length - 1; i >=0; i--){
+//     inverso += palabra[i];
+// }
+// if (inverso == palabra){
+//     document.write("es capicua");
+// }else{
+//     document.write("no es capicua");
+// }
+
+// let coches = ["BMW", "Audi", "Seat", "Volvo"];
+//  for (let a of coches){
+//     document.write(a + "<br>");
+//  }
+
+//  for (let i = 0; i < coches.length; i++){
+//     document.write(coches[i]);
+//  }
+
+
+ //POP quita detras
+ //UNSHIFT añade delante
+ //SHIFT quita delante
+//  //PUSH añade detras
+
+//  let programas = ["HTML", "CSS", "JS"];
+//  console.log(programas);
+
+// //  programas.push("PHYTON");
+
+// //  console.log(programas);
+
+// //  programas.pop();
+// //  console.log(programas);
+
+// //  programas.unshift("Word");
+// //  console.log(programas);
+
+// //  programas.shift();
+// //  console.log(programas);
+
+// console.log(programas.splice(2, 1, "Excel", "Word"));
+// console.log(programas);
+
+
+
+// let numeros = [31, 18, 10, 11, 32, 17];
+
+//  for (let a of numeros){
+//     document.write(a + "<br>");
+//  }
+
+//  for (let i = 0; i < numeros.length; i++){
+//     document.write(numeros[i] + " ");
+//  }
+
+//   let colores = ["red", "green", "blue"];
+// //  for(let k of colores){
+// //     document.write(k + " ");
+// //  }
+
+// let colores2 = [];
+
+// for(color of colores){
+//     colores2.push(color);
+// }
+
+
+// console.log(colores2);
 
